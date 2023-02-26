1c6f09b27b041c1202b3a398011a9411f3dd67ca
dengwenj <927561237@qq.com>
2023-02-26 20:07:10 +0800

    护工管理关联老人

--- src/services/careWorker.ts
+++ src/services/careWorker.ts
@@ -2,6 +2,7 @@
  * 护工管理
  */
 import mysql from "../utils/mysql"
+import pool from '../dataBase'
 
 import type { ICareWorkerInfo, Page } from "../global/types" 
 
@@ -26,8 +27,45 @@ class CareWorkerServices {
 
   // 分页
   async pageCareWorkerS(data: Page) {
-    const res = mysql.actionPage('care_worker', data)
-    return res
+    const { current, pageSize, oldPersonId, careWorkerName } = data
+
+    let sql = ''
+    if (oldPersonId !== undefined && oldPersonId !== null && oldPersonId !== '') {
+      sql = `AND o.id = ${oldPersonId} `
+    }
+    let sql1 = ''
+    if (careWorkerName !== undefined && careWorkerName !== null && careWorkerName !== '') {
+      sql1 = `AND c.careWorkerName like '%${careWorkerName}%'  `
+    }
+    
+    // 病例，老人名字允许模糊查询
+    const statement = `
+      SELECT c.*, o.oldPersonName, o.gender, o.age
+			FROM care_worker c, old_person o 
+			WHERE c.oldPersonId = o.id 
+      ${sql}
+      ${sql1}
+      LIMIT ${((current || 1) - 1) * (pageSize || 10)},${pageSize || 10}
+    `
+    
+    const statementTotal = `
+      SELECT COUNT(*)
+			FROM care_worker c, old_person o 
+			WHERE c.oldPersonId = o.id 
+      ${sql}
+      ${sql1}
+    `
+    try {
+      const res = await pool.execute(statement)
+      const res1: any = await pool.execute(statementTotal)
+      
+      return {
+        data: res[0],
+        total: res1[0][0]['COUNT(*)']
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
